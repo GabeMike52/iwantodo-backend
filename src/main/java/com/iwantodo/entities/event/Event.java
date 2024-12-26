c2b79e9e13ae8ccf0477f39f69729e681761b421
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2024-12-25 21:20:02 -0300

    Entities created, database connection added but contains sensitive data.

--- src/main/java/com/iwantodo/entities/event/Event.java
+++ src/main/java/com/iwantodo/entities/event/Event.java
@@ -1,4 +1,39 @@
 package com.iwantodo.entities.event;
 
+import com.iwantodo.entities.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "events")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long eventId;
+    private String title;
+
+    @ManyToOne
+    private User owner;
+
+
+
+
+    public Long getEventId() {
+        return eventId;
+    }
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getOwner() {
+        return owner;
+    }
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
--- src/main/java/com/iwantodo/entities/user/User.java
+++ src/main/java/com/iwantodo/entities/user/User.java
@@ -1,7 +1,43 @@
 package com.iwantodo.entities.user;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+
+
+
+
+    public Long getId() {
+        return userId;
+    }
+    public void setId(Long userId) {
+        this.userId = userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
