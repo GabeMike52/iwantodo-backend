bd7f1a6e735693c1381f757023acc562e50ca70e
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2024-12-26 21:17:23 -0300

    Removed unused import from the UserRepository.

--- src/main/java/com/iwantodo/repositories/UserRepository.java
+++ src/main/java/com/iwantodo/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.iwantodo.repositories;
 
 import com.iwantodo.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
