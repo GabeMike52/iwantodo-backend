6c4e81f9d497f0280add8a9392f79f20cbb5bd54
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2024-12-27 10:17:43 -0300

    Added customized exception handling if a user already exists. Added logging for the exception and the service class.

--- src/main/java/com/iwantodo/infra/exception/ErrorMessages.java
+++ src/main/java/com/iwantodo/infra/exception/ErrorMessages.java
@@ -5,7 +5,8 @@ public enum ErrorMessages {
     EMAIL_NOT_VALID("Email using invalid format"),
     EMAIL_REQUIRED("Email is required"),
     PASSWORD_REQUIRED("Password is required"),
-    PASSWORD_LENGTH("Password must be at least 8 chars");
+    PASSWORD_LENGTH("Password must be at least 8 chars"),
+    USER_ALREADY_EXISTS("A user with this username already exists");
 
     private final String message;
     ErrorMessages(String message) {
--- src/main/java/com/iwantodo/services/user/CreateUserService.java
+++ src/main/java/com/iwantodo/services/user/CreateUserService.java
@@ -2,9 +2,13 @@ package com.iwantodo.services.user;
 
 import com.iwantodo.entities.user.User;
 import com.iwantodo.entities.user.UserDTO;
+import com.iwantodo.infra.exception.ErrorMessages;
+import com.iwantodo.infra.exception.UserNotValidException;
 import com.iwantodo.repositories.UserRepository;
 import com.iwantodo.services.Command;
 import com.iwantodo.validators.UserValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,7 @@ import java.util.Optional;
 public class CreateUserService implements Command<User, UserDTO> {
     private final PasswordEncoder encoder;
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserService.class);
     public CreateUserService(PasswordEncoder encoder,
                              UserRepository userRepository) {
         this.encoder = encoder;
@@ -23,12 +28,13 @@ public class CreateUserService implements Command<User, UserDTO> {
 
     @Override
     public ResponseEntity<UserDTO> execute(User user) {
+        logger.info("Executing " + getClass() + " user: " + user);
         UserValidator.execute(user);
         Optional<User> userOptional = Optional.ofNullable(userRepository.findUserByUsername(user.getUsername()));
         if(userOptional.isEmpty()) {
             User savedUser = userRepository.save(new User(user.getUsername(), user.getEmail(), encoder.encode(user.getPassword())));
             return ResponseEntity.ok(new UserDTO(savedUser));
         }
-        throw new RuntimeException("User already exists");
+        throw new UserNotValidException(ErrorMessages.USER_ALREADY_EXISTS.getMessage());
     }
 }
